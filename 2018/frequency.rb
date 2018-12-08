545c2d4af35930105ccd407b776e3e718380f60e
Jacob Sherwood <sport@fizzix.org>
2018-12-07 19:15:20 -0500

    Find first repeating frequency

--- 2018/frequency.rb
+++ 2018/frequency.rb
@@ -6,8 +6,29 @@ unless ARGV.length > 0
 end
 
 freq_file = ARGV[0]
+verbose = ARGV[1] == '-v'
 
 frequency = 0
-File.read(freq_file).each_line { |l| frequency += l.to_i }
+frequencies = []
+twice = nil
+
+iterations = 0
+data = File.read(freq_file).split("\n").map { |l| l.to_i }
+loop do
+  break if twice
+  data.each do |change|
+    cur = frequency
+    frequency += change
+    unless twice
+      iterations += 1
+      twice = frequency if frequencies.include?(frequency)
+    end
+    frequencies << frequency
+    puts "Current frequency #{cur}, change of #{change}; resulting frequency #{frequency}" if verbose
+    puts "twice: #{twice}" if verbose
+    break if twice
+  end
+end
 
 puts "Final frequency: #{frequency}"
+puts "First frequency to reach twice: #{twice} (#{iterations})"
